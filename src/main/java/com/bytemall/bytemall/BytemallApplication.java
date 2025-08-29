78e80f7f36957043f690078ddec192982df8cc07
典风 <fanfeng151105@gmail.com>
2025-08-29 09:36:22 +0800

    feat: Initial project setup with Spring Boot

---
+++ src/main/java/com/bytemall/bytemall/BytemallApplication.java
@@ -0,0 +1,13 @@
+package com.bytemall.bytemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytemallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bytemall/bytemall/BytemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytemall.bytemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
